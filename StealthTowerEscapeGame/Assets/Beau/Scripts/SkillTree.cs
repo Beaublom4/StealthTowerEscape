b6f59dead879c36276ba24561c1360dafb756714
Beaublom4 <Beaublom4@gmail.com>
2021-01-26 01:21:20 +0100

    Begin skill tree

---
+++ StealthTowerEscapeGame/Assets/Beau/Scripts/SkillTree.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillTree : MonoBehaviour
+{
+    [SerializeField] float sliderSpeed;
+    [SerializeField] SkillTreeBranch currentBranchScript;
+
+    bool increaseSlider;
+    private void Update()
+    {
+        if(increaseSlider == true)
+        {
+            currentBranchScript.branchSlider.value += sliderSpeed * Time.deltaTime;
+            if(currentBranchScript.branchSlider.value == 1)
+            {
+                if (currentBranchScript.unlocked)
+                    return;
+                currentBranchScript.UnlockBranch();
+            }
+        }
+    }
+    public void OnButtonDown(SkillTreeBranch branchScript)
+    {
+        currentBranchScript = branchScript;
+        if (currentBranchScript.unlocked || !currentBranchScript.preBranch.unlocked)
+            return;
+
+        increaseSlider = true;
+    }
+    public void OnButtonUp()
+    { 
+
+    }
+}
---
+++ StealthTowerEscapeGame/Assets/Beau/Scripts/SkillTreeBranch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillTreeBranch : MonoBehaviour
+{
+    public Slider branchSlider;
+
+    [SerializeField] public SkillTreeBranch preBranch;
+    [SerializeField] public bool unlocked;
+
+    public void UnlockBranch()
+    {
+        unlocked = true;
+        print("Unlucked");
+    }
+}
