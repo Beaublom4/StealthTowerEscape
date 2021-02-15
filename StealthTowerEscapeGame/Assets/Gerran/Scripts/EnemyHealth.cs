da93347e99ac39a3ed97ffe9d5cdbd6516df1bd2
gerranR <60340232+gerranR@users.noreply.github.com>
2021-02-15 10:38:16 +0100

    melee

---
+++ StealthTowerEscapeGame/Assets/Gerran/Scripts/EnemyHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    public float health;
+
+    public void Update()
+    {
+        if(health <=0.1)
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
---
+++ StealthTowerEscapeGame/Assets/Gerran/Scripts/Melee.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour
+{
+
+    public float range, damage, backstabDamage;
+    public Transform crTrans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(crTrans.position, transform.forward, out hit, range))
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                print("hit");
+                
+                if (hit.transform.tag == "Backstab")
+                {
+                    print("backstab");
+                    hit.transform.gameObject.GetComponentInParent<EnemyHealth>().health -= backstabDamage;
+                }
+
+                else if (hit.transform.tag == "Enemy")
+                {
+                    print("normal");
+                    hit.transform.gameObject.GetComponent<EnemyHealth>().health -= damage;
+                }
+
+                
+            }
+        }
+    }
+}
