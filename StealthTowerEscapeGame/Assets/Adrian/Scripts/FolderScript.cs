a0a2f5001764f4452c6a34cea1ff72d2461f9792
DeimosPlays <deimosplayss@gmail.com>
2021-02-16 01:00:29 +0100

    i

---
+++ StealthTowerEscapeGame/Assets/Adrian/Scripts/FolderScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FolderScript : MonoBehaviour
+{
+    [Header("Objects")]
+    public GameObject folderMenu;
+
+    [Header("Variables")]
+    public float openTime = 1f;
+
+    void Start()
+    {
+        OpenFolder();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OpenFolder()
+    {
+        folderMenu.SetActive(false);
+
+        StartCoroutine("ShowMenu");
+    }
+
+    private IEnumerator ShowMenu()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(openTime);
+
+            folderMenu.SetActive(true);
+        }
+    }
+}
