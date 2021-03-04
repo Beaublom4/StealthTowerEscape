7a34bb7217d8aaae3ed967c8928c67264b1d8ebc
gerranR <60340232+gerranR@users.noreply.github.com>
2021-03-04 22:08:12 +0100

    fixt interect

--- StealthTowerEscapeGame/Assets/Gerran/Scripts/FlyingObj.cs
+++ StealthTowerEscapeGame/Assets/Gerran/Scripts/FlyingObj.cs
@@ -6,7 +6,13 @@ public class FlyingObj : MonoBehaviour
 {
     public float range, velosity, speed;
     public Transform crTrans;
-    
+
+    public Transform pickupTrans;
+    public GameObject dest;
+
+    public bool hasPickedUp;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,21 +26,36 @@ public class FlyingObj : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(crTrans.position, transform.forward , out hit, range))
         {
-            if(hit.transform.tag == "Object" && Input.GetButton("Fire1"))
+            if(hit.transform.tag == "Object")
             {
-                if (hit.transform.GetComponent<Interect>().hasPickedUp == true)
+                print("hit");
+                if(Input.GetButton("Fire2"))
                 {
-                    print("throw test");
-                    hit.transform.parent = null;
+                    print("getem");
+                    hasPickedUp = true;
                     hit.transform.GetComponent<Rigidbody>().useGravity = true;
-                    hit.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-                    hit.transform.GetComponent<Interect>().hasPickedUp = false;
-                    hit.transform.GetComponent<Rigidbody>().velocity = transform.forward * (velosity * speed) * Time.deltaTime;
+                    hit.transform.position = pickupTrans.position;
+                    hit.transform.parent = dest.transform;
+                    hit.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
                 }
 
-                else
+                if (Input.GetButton("Fire1"))
                 {
-                    hit.rigidbody.velocity = transform.forward * (velosity * speed) * Time.deltaTime;
+                    if (hasPickedUp == true)
+                    {
+                        print("throw test");
+                        hit.transform.parent = null;
+                        hit.transform.GetComponent<Rigidbody>().useGravity = true;
+                        hit.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                        hasPickedUp = false;
+                        hit.transform.GetComponent<Rigidbody>().velocity = transform.forward * (velosity * speed) * Time.deltaTime;
+                    }
+
+                    else
+                    {
+                        hit.rigidbody.velocity = transform.forward * (velosity * speed) * Time.deltaTime;
+                    }
                 }
             }
 
