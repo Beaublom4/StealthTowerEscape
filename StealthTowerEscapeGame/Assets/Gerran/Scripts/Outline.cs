ce49f2fc2c4f2fa9ac03aff15dd1320dd643fcde
gerranR <60340232+gerranR@users.noreply.github.com>
2021-03-11 16:37:45 +0100

    outline

---
+++ StealthTowerEscapeGame/Assets/Gerran/Scripts/Outline.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outline : MonoBehaviour
+{
+    [SerializeField] private Material outlineMaterial;
+    [SerializeField] private float outlineScaleFactor;
+    [SerializeField] private Color outlineColor;
+    private Renderer outlineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        outlineRenderer = CreateOutline(outlineMaterial, outlineScaleFactor, outlineColor);
+        outlineRenderer.enabled = true;
+    }
+
+    Renderer CreateOutline(Material outlineMat, float scaleFector, Color color)
+    {
+        GameObject outlineObject = Instantiate(this.gameObject, transform.position, transform.rotation, transform);
+        Renderer rend = outlineObject.GetComponent<Renderer>();
+
+        outlineObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        outlineObject.tag = "Untagged";
+
+        rend.material = outlineMat;
+        rend.material.SetColor("_OutlineColor", color);
+        rend.material.SetFloat("_Scale", scaleFector);
+        rend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+        outlineObject.GetComponent<Outline>().enabled = false;
+        outlineObject.GetComponent<Collider>().enabled = false;
+
+        rend.enabled = false;
+
+        return rend;
+    }
+}
